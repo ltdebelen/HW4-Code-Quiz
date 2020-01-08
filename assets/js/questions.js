c91668d62f4913f7b22156c8bb2614cd89b205f5
Lawrence Adrian De Belen <ltdebelen@gmail.com>
2020-01-08 17:35:03 -0600

    Fixing file structure, removing extra html file.

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,8 +1,13 @@
-var questions = [
+let questions = [
   {
     title: "Commonly used data types DO NOT include:",
     choices: ["strings", "booleans", "alerts", "numbers"],
     answer: "alerts"
+  },
+  {
+    title: "The condition in an if / else statement is enclosed within ____.",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses"
   },
   {
     title: "The condition in an if / else statement is enclosed within ____.",
