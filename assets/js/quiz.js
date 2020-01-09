ec0e7fa0805a89694f9ad9bbb95a0a44bf64974e
Lawrence Adrian De Belen <ltdebelen@gmail.com>
2020-01-08 21:28:17 -0600

    Adding function for user's choice validation. Handled array out of bounds error

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -6,16 +6,6 @@ const pEl = document.getElementById("paragraph");
 const btnStartGame = document.getElementById("start-game");
 const btnHighScore = document.getElementById("high-score");
 
-// Other Variables
-let questionCounter = 0;
-
-btnStartGame.addEventListener("click", startGame);
-
-function startGame() {
-  divEl.style.display = "none";
-  getQuestion();
-}
-
 let questions = [
   {
     title: "Commonly used data types DO NOT include:",
@@ -26,38 +16,57 @@ let questions = [
     title: "The condition in an if / else statement is enclosed within ____.",
     choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
     answer: "parentheses"
-  },
-  {
-    title: "The condition in an if / else statement is enclosed within ____.",
-    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-    answer: "parentheses"
   }
 ];
 
+// Other Variables
+let questionCounter = 0;
+const questionsLength = questions.length;
+
+btnStartGame.addEventListener("click", startGame);
+
+function startGame() {
+  divEl.style.display = "none";
+  getQuestion();
+}
+
 function getQuestion() {
-  // Increment Question Counter
-  questionCounter++;
+  if (questionCounter >= questionsLength) {
+  } else {
+    // get array value
+    let title = questions[questionCounter].title;
+    let choices = questions[questionCounter].choices;
 
-  // get array value
-  let title = questions[questionCounter].title;
-  let choices = questions[questionCounter].choices;
-
-  // build HTML, append Elemetns
-  let questionHTML = `
+    // build HTML, append Elemetns
+    let questionHTML = `
   <div class="starter-template" id="main-div">
-    <h1 id="header">Question: ${questionCounter}</h1>
+    <h1 id="header">Question ${questionCounter + 1}:</h1>
     <p id="paragraph" class="lead">
     ${title}
-    </p>
-  </div>`;
+    </p>`;
 
-  // loop through choices
-  choices.forEach(choice => {
-    questionHTML += `
-    <button class="btn btn-outline-primary" value="${choice}">${choice}</button>
+    // loop through choices
+    choices.forEach(choice => {
+      questionHTML += `
+    <button class="btn btn-outline-primary" value="${choice}" onclick="validateChoice(${questionCounter}, '${choice}')">${choice}</button>
     `;
-  });
+    });
 
-  // append elements
-  mainSectionEl.innerHTML = questionHTML;
+    // append elements
+    mainSectionEl.innerHTML = questionHTML;
+
+    questionCounter++;
+  }
+}
+
+function validateChoice(questionCounter, choice) {
+  let counter = questionCounter;
+  let userChoice = choice;
+  let answer = questions[counter].answer;
+
+  if (userChoice == answer) {
+    console.log("YOU ARE CORRECT");
+  }
+
+  getQuestion();
 }
